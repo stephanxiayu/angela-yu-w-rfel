dcd1c659e1ae0aa08e02360ffee02e6c9496df48
stephanxiayu <Stephan.Entwickler@gmail.com>
2021-10-19 19:39:20 +0200

    Changes to be committed: 	modified:   android/app/build.gradle 	modified:   android/app/src/debug/AndroidManifest.xml 	modified:   android/app/src/main/AndroidManifest.xml 	modified:   android/app/src/main/kotlin/com/example/angela1/MainActivity.kt 	modified:   android/app/src/profile/AndroidManifest.xml 	modified:   lib/main.dart

--- android/app/src/main/kotlin/com/example/angela1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/angela1/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.angela1
+package com.sg_technology.angela1
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -21,51 +21,51 @@ class MyApp extends StatelessWidget {
 }
 
 class Dice extends StatefulWidget {
-
-
   @override
   State<Dice> createState() => _DiceState();
 }
 
 class _DiceState extends State<Dice> {
- int leftDiceNumber=1;
- int rightDiceNumber=1;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
 
- 
-  void changeWurfel(){
+  void changeWurfel() {
     setState(() {
-              leftDiceNumber= Random().nextInt( 6)+1;
-                rightDiceNumber= Random().nextInt( 6)+1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(elevation: 9,
+      appBar: AppBar(
+        elevation: 9,
         title: Center(child: Text("Würfel")),
-      backgroundColor: Colors.red,),
+        backgroundColor: Colors.red,
+      ),
       backgroundColor: Colors.red,
-      body: FlatButton(onPressed: (){changeWurfel();},
+      body: FlatButton(
+        onPressed: () {
+          changeWurfel();
+        },
         child: Center(
-          child: Row(children: [
-            Expanded(
-              
-               
+          child: Row(
+            children: [
+              Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Image.asset("lib/images/dice$leftDiceNumber.png"),
-                ),),
-            
-            Expanded(
-            
+                ),
+              ),
+              Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Image.asset("lib/images/dice$rightDiceNumber.png"),
-                ),),
-            
-          ],),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
